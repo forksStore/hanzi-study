e3a631e78ada9fbcb83c5d8f0cc8263b467bf385
dhjz <909514844@qq.com>
2025-06-21 13:45:29 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,177 @@
+// window.bgAudio = new Audio('audio/bg.mp3')
+
+Vue.createApp({
+  data() {
+    return {
+      flag: 1,
+      bgPlay: true,
+      notStart: true,
+      list: window.dataList,
+      message: 'Hello Vue!',
+      lockInd: 0,
+      currItem: null,
+      currInd: 0,
+      randomList: [],
+      hanziShow: true,
+      cardShow: false,
+      gameShow: false,
+      gameStep: [],
+      gameType: 'listen', // listen fill stroke draw
+      gameStatus: null, // init playing end
+      strokeStatus: null, // [0, 1]
+      drawStatus: null, // 1 - 100
+      drawImg: null,
+      audioBg: null
+    }
+  },
+  created() {
+    this.lockInd = getStorage('lockInd') || 0
+    this.bgPlay = getStorage('bgPlay') === null ? true : getStorage('bgPlay')
+    if (!this.bgPlay) this.notStart = false
+  },
+  mounted() {
+    this.audioBg = document.getElementById('audioBg')
+    loadScripts()
+    document.querySelector(`#hanzi-list > .hanzi-item:nth-of-type(${this.lockInd + 1})`).scrollIntoView({ behavior: 'smooth', block: 'center' })
+  },
+  methods: {
+    speakText: speakText,
+    toggleBg() {
+      this.bgPlay = !this.bgPlay
+      setStorage('bgPlay', this.bgPlay)
+      if (this.bgPlay) {
+        this.audioBg.play()
+      } else {
+        this.audioBg.pause()
+      }
+    },
+    goTop() {
+      document.getElementById('hanzi-list').scrollTo({ top: 0, behavior: 'smooth'});
+    },
+    startApp() {
+      this.notStart = false
+      this.bgPlay && this.audioBg.play()
+    },
+    goBack(step) {
+      playAudio('click')
+      this.hanziShow = step == 1;
+      this.cardShow = step == 2;
+      this.gameShow = false;
+      this.gameStatus = null
+      this.strokeStatus = null
+      this.drawStatus = null
+      if (step == 1 && this.bgPlay) this.audioBg.play()
+    },
+    hanziClick(item, ind) {
+      if (this.lockInd < ind) return playAudio('lock') && myAlert('小朋友请先学习前面的汉字哦~', 10);
+      this.audioBg.pause()
+      this.currItem = item;
+      this.currInd = ind;
+      this.gameStep = [];
+      // 随机抽取ind周围前后20个之内的3个汉字
+      let temp = window.dataList
+        .filter((x, i) => i >= ind - 20 && i <= ind + 20 && x.w != item.w)
+        .sort(() => Math.random() - 0.5).slice(0, 3)
+      // temp随机一个位置插入currItem
+      temp.splice(Math.floor(Math.random() * temp.length), 0, item)
+      this.randomList = temp.map(item => ({ ...item }));
+      this.hanziShow = false;
+      this.cardShow = true;
+      setTimeout(() => this.readItem(item), 1000)
+    },
+    readItem(item) {
+      item = item || this.currItem;
+      console.log(item);
+      speakText(item.w + ', ' + item.p + '的' + item.w)
+    },
+    goGame(type) {
+      if (this.lockInd == this.currInd && ((type === 'listen' && this.gameStep.length < 1)
+        || (type === 'draw' && this.gameStep.length < 2))) return playAudio('gamelock')
+      this.cardShow = false;
+      this.gameType = type;
+      this.gameShow = true
+      this.randomList.forEach(x => x.status = '')
+      setTimeout(() => {
+        playAudio(`game-${type}`)
+        if (type === 'stroke') {
+          this.strokeStatus = null
+          listHanzi(document.getElementById('stroke-list'), this.currItem.w, 46)
+          aniHanzi(document.getElementById('stroke-ani'), this.currItem.w, 160)
+          writeHanzi(document.getElementById('stroke-write'), this.currItem.w, 180, (_, finish, total) => {
+            this.strokeStatus = [finish, total]
+            if (finish === total) {
+              playAudio('correct')
+              playAudio('correct-stroke')
+              if (!this.gameStep.includes(1)) this.gameStep.push(1)
+            }
+          })
+        }
+        if (type === 'listen') {
+          setTimeout(() => speakText(this.currItem.w, 3), 2600)
+        }
+        if (type === 'draw') {
+          setTimeout(() => speakText(this.currItem.w, 3), 3400)
+          let len = 33
+          // 随机一个
+          this.drawImg = `./img/draw/draw${Math.floor(Math.random() * len + 1)}.webp`
+          this.drawStatus = 10
+        }
+      }, 500)
+    },
+    listenClick(item) {
+      this.randomList.forEach(x => x.status = '')
+      if (this.gameStatus == 'correct') return
+      if (item.w == this.currItem.w) {
+        console.log(6666666666);
+        playAudio('correct')
+        playAudio('correct-listen')
+        item.status = 'correct'
+        this.gameStatus = 'correct'
+        if (!this.gameStep.includes(2)) this.gameStep.push(2)
+      } else {
+        playAudio('wrong')
+        item.status = 'wrong'
+        this.gameStatus = 'wrong'
+      }
+    },
+    drawClick(item) {
+      this.randomList.forEach(x => x.status = '')
+      console.log('drawClick..........');
+      if (this.drawStatus === 100) {
+        return
+      }
+      if (item.w == this.currItem.w) {
+        playAudio('correct')
+        item.status = 'correct'
+        let temp = this.drawStatus + 10
+        this.drawStatus = temp > 100 ? 100 : temp
+        if (this.drawStatus === 100) {
+          playAudio('correct-stroke')
+          this.gameStatus = 'correct'
+          if (!this.gameStep.includes(3)) this.gameStep.push(3)
+          this.addLockInd()
+        }
+
+      } else {
+        playAudio('wrong1')
+        item.status = 'wrong'
+        let temp = this.drawStatus - 10
+        this.drawStatus = temp < 0 ? 0 : temp
+      }
+    },
+    addLockInd() {
+      this.lockInd += 1
+      setStorage('lockInd', this.lockInd)
+    },
+    flagClick() {
+      this.flag++
+      if (this.flag == 10) {
+        let ind = window.prompt('请输入解锁关卡')
+        if (ind && ind.trim() && !isNaN(ind)) {
+          setStorage('lockInd', parseInt(ind))
+          location.reload()
+        }
+      }
+    }
+  },
+}).mount('#app')
---
+++ js/utils.js
@@ -0,0 +1,142 @@
+// # 浏览器-storage封装
+function getStorage(key) {
+  let result = localStorage.getItem(key)
+  try {
+    return JSON.parse(result)
+  } catch {
+    return result
+  }
+}
+function setStorage(key, val) {
+  if (!val && val !== 0 && val !== false) return localStorage.removeItem(key)
+  localStorage.setItem(key, JSON.stringify(val))
+}
+function delStorage(key) {
+  localStorage.removeItem(key)
+}
+
+function playAudio(type) {
+  return new Audio(`./audio/${type}.mp3`).play()
+}
+
+function myAlert(msg, timeout) {
+  if (!timeout) return alert(msg)
+  setTimeout(() => alert(msg), timeout)
+}
+
+function loadScripts() {
+  const script = document.createElement('script')
+  script.src = 'js/lib/dataWriter.js'
+  document.body.appendChild(script)
+}
+
+ function speakText(text, times) {
+    if (window.speechSynthesis) {
+        times = times || 1;
+        const utterance = new SpeechSynthesisUtterance(new Array(times).fill(text).join("。 "));
+        utterance.lang = "zh-CN";
+        utterance.rate = 0.7; // 语速（0.1-10，默认1）
+        utterance.pitch = 1; // 音高（0-2，默认1）
+        // volume：音量（0-1，默认1）
+        speechSynthesis.speak(utterance);
+    } else {
+        alert("您的浏览器不支持语音合成功能。");
+    }
+}
+
+function listHanzi(target, val, size) {
+  console.log('listHanzi..', target, val, size);
+  target.innerHTML = ''
+  size = size || 75
+  val && val.trim() && myCharDataLoader(val, (data) => {
+    for (var i = 0; i < data.strokes.length; i++) {
+      var strokesPortion = data.strokes.slice(0, i + 1);
+      renderFanningStrokes(target, strokesPortion, size);
+    }
+  })
+}
+
+function renderFanningStrokes(target, strokes, size) {
+  var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.style = `width: ${size}px; height: ${size}px; border: 1px solid #EEE; margin-right: 3px`
+  target.appendChild(svg);
+  var group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+  var transformData = HanziWriter.getScalingTransform(size, size);
+  group.setAttributeNS(null, 'transform', transformData.transform);
+  svg.appendChild(group);
+
+  strokes.forEach(function (strokePath, ind) {
+    var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttributeNS(null, 'd', strokePath);
+    path.style.fill = '#555';
+    if (ind === strokes.length - 1) path.style.fill = '#ff6347'; // 最后一个笔画不填充颜色（即不闭合路径
+    group.appendChild(path);
+  });
+}
+
+function aniHanzi(target, val, size) {
+  target.innerHTML = ''
+  size = size || 150
+  let writer = HanziWriter.create(target, val, {
+    charDataLoader: myCharDataLoader,
+    width: 150,
+    height: 150,
+    showCharacter: false,
+    padding: 5,
+    strokeAnimationSpeed: 1, // 动画速度
+    delayBetweenStrokes: 500, // 每个笔画之间的间隔时间 ms
+  });
+  writer.loopCharacterAnimation();
+}
+
+function writeHanzi(target, val, size, callback) {
+  target.innerHTML = ''
+  size = size || 150
+  let writer = HanziWriter.create(target, val, {
+    charDataLoader: myCharDataLoader,
+    width: 150,
+    height: 150,
+    showCharacter: false,
+    showHintAfterMisses: 2,
+    padding: 5,
+  });
+  writer.quiz({
+    onCorrectStroke: function(strokeData) {
+      console.log(strokeData);
+      callback && callback(strokeData, strokeData.strokeNum + 1, strokeData.strokeNum + 1 + strokeData.strokesRemaining);
+    },
+    // onComplete: function(summaryData) {}
+  });
+}
+
+function myCharDataLoader(char, onComplete) {
+  if (writerData[char]) {
+      console.log(`匹配到了本地${char}的笔画数据`);
+      onComplete(writerData[char]);
+    } else {
+      console.log(`本地没有<b> ${char} </b>的笔画数据，正在加载...`);
+      HanziWriter.loadCharacterData(char).then(data => {
+        if (!data) return
+        writerData[char] = data;
+        // 最好也本地缓存一下新的笔画数据，不然每次都要重新加载一次
+        reso(writerData[char]);
+      }); 
+    }
+}
+
+// var writer = HanziWriter.create('docs-target-7', '测', {
+//   width: 150,
+//   height: 150,
+//   showCharacter: false,
+//   showHintAfterMisses: 3,
+//   padding: 5,
+//   strokeAnimationSpeed: 1, // 动画速度
+//   delayBetweenStrokes: 500, // 每个笔画之间的间隔时间 ms
+// });
+// writer.quiz();
+// document.getElementById('docs-target-7-button').addEventListener('click', function () {
+//   writer.quiz();
+// });
+// document.getElementById('animate-button').addEventListener('click', function() {
+//   writer.animateCharacter();
+// });
